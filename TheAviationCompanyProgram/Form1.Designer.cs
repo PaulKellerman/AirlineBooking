7029020105536bcb79b0a03bf2de31aecdff1f72
Влад Пылайкин <vlad2005p2005@mail.ru>
2024-06-05 17:39:39 +0300

    Начало создания интерфейса

--- TheAviationCompanyProgram/Form1.Designer.cs
+++ TheAviationCompanyProgram/Form1.Designer.cs
@@ -28,20 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menubutton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // menubutton
+            // 
+            this.menubutton.Location = new System.Drawing.Point(160, 303);
+            this.menubutton.Name = "menubutton";
+            this.menubutton.Size = new System.Drawing.Size(170, 61);
+            this.menubutton.TabIndex = 1;
+            this.menubutton.Text = "button2";
+            this.menubutton.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::TheAviationCompanyProgram.Properties.Resources.airlane;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(207, 140);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(608, 525);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(532, 401);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menubutton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button menubutton;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
